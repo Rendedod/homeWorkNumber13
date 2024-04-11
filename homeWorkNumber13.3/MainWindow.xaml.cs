01ab6a557cd70fbb7a27ff455bdf6123c5640b66
Rended <ilyaromanovich2024@mail.ru>
2024-04-12 03:57:09 +0700

    Добавьте файлы проекта.

---
+++ homeWorkNumber13.3/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace homeWorkNumber13._3
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        int numberConvert;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void myText_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            char text = e.Text[0];
+            if (!Char.IsDigit(text) && text != '-')
+                e.Handled = true;
+        }
+
+        private void myText_KeyUp(object sender, KeyEventArgs e)
+        {
+            try
+            {
+                numberConvert = Convert.ToInt32(myText.Text);
+                myText.Text = numberConvert.ToString();
+            }
+            catch
+            {
+                myText.Text = myText.Text.Replace("--", "-");
+            }
+        }
+
+        private void RadioButton_Checked_1(object sender, RoutedEventArgs e)
+        {
+            decision.Content = Convert.ToString(numberConvert, 2) ?? "0";
+        }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            decision.Content = Convert.ToString(numberConvert, 8) ?? "0";
+        }
+    }
+}
---
+++ homeWorkNumber13/MainWindow.xaml.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace homeWorkNumber13
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        char? mathematicalAction;
+        double? firstNumberComputing, secendNumberComputing;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void firstNumber_TextChanged(object sender, TextChangedEventArgs e)
+        {   firstNumber.Text = deliteText(firstNumber.Text);
+
+            if (firstNumber.Text != string.Empty && !firstNumber.Text.Contains('-'))
+                firstNumberComputing = Convert.ToDouble(firstNumber.Text);
+            else if(firstNumber.Text.Length > 1 && firstNumber.Text != string.Empty)
+                firstNumberComputing = Convert.ToDouble(firstNumber.Text);
+            else
+                firstNumberComputing = null;
+
+            decision();
+
+            firstNumber.SelectionStart = firstNumber.Text.Length;
+            firstNumber.SelectionLength = 0;
+        }
+
+        private void secendNumber_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            secendNumber.Text = deliteText(secendNumber.Text);
+
+            if (secendNumber.Text != string.Empty && !secendNumber.Text.Contains('-'))
+                secendNumberComputing = Convert.ToDouble(secendNumber.Text);
+            else if (secendNumber.Text.Length > 1 && secendNumber.Text != string.Empty)
+                secendNumberComputing = Convert.ToDouble(secendNumber.Text);
+            else
+                secendNumberComputing = null;
+
+            decision();
+
+            secendNumber.SelectionStart = secendNumber.Text.Length;
+            secendNumber.SelectionLength = 0;
+        }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            mathematicalAction = '+';
+            decision();
+        }
+
+        private void RadioButton_Checked_1(object sender, RoutedEventArgs e)
+        {
+            mathematicalAction = '-';
+            decision();
+        }
+
+        private void RadioButton_Checked_2(object sender, RoutedEventArgs e)
+        {
+            mathematicalAction = '*';
+            decision();
+        }
+
+        private void RadioButton_Checked_3(object sender, RoutedEventArgs e)
+        {
+            mathematicalAction = '/';
+            decision();
+        }
+
+        private void RadioButton_Checked_4(object sender, RoutedEventArgs e)
+        {
+            mathematicalAction = '%';
+            decision();
+        }
+
+        private void RadioButton_Checked_5(object sender, RoutedEventArgs e)
+        {
+            mathematicalAction = '^';
+            decision();
+        }
+
+        private string deliteText(string text)
+        {
+            if(text != string.Empty)
+            {
+                text = text.Replace('.', ',');
+
+                text = text[0] + text.Substring(1).Replace("-", "");
+
+                // Запрещает ставить в начало более одного 0 и одной ','
+                while (text[0] == '0' && text.Length > 1)
+                    text = text.Substring(1);
+
+                if (text[0] == ',')
+                    text = 0 + text;
+                else if (text.Length > 2 && text[0] == '-' && text[1] == '0')
+                    text = "-" + text.Substring(2);
+
+                // Запрещает ставить более одной ',' в любом месте ткста
+                if (text.Contains(","))
+                {
+                    string textSupport = text.Substring(0, text.Length - 1);
+                    if (textSupport.Contains(',') && text[text.Length - 1] == ',')
+                        text = textSupport;
+                }
+
+                if(text.Replace("-", "").Replace(",", "").Length >= 27)
+                {
+                    if (text.Contains(',') && text.Contains('-'))
+                        text = text[0] + text.Substring(1, 27);
+                    else if (text.Contains(',') || text.Contains('-'))
+                        text = text.Substring(0, 28);
+                    else
+                        text = text.Substring(0, 27);
+                }
+
+                return Regex.Replace(text, "[^0-9,.-]", "").Replace("-,", "-0,");
+
+            }
+            else 
+                return string.Empty;
+        }
+
+        private void decision()
+        {
+            if (firstNumberComputing != null && secendNumberComputing != null && mathematicalAction != null)
+            {
+                switch (mathematicalAction)
+                {
+                    case '+':
+                        ansvered.Content = Convert.ToString(firstNumberComputing + secendNumberComputing);
+                        break;
+
+                    case '-':
+                        ansvered.Content = Convert.ToString(firstNumberComputing - secendNumberComputing);
+                        break;
+
+                    case '*':
+                        ansvered.Content = Convert.ToString(firstNumberComputing * secendNumberComputing);
+                        break;
+
+                    case '/':
+                        ansvered.Content = Convert.ToString(firstNumberComputing / secendNumberComputing);
+                        break;
+
+                    case '%':
+                        ansvered.Content = Convert.ToString(firstNumberComputing % secendNumberComputing);
+                        break;
+
+                    case '^':
+                        ansvered.Content = Convert.ToString(Math.Pow(firstNumberComputing ?? 0,secendNumberComputing ?? 0));
+                        break;
+                }
+            }
+            else ansvered.Content = "***";
+        }
+
+    }
+}
+
+
